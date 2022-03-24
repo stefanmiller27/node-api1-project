56f2eb163571bd99f5d293fc957de82764ae7897
Stefan Miller <stefanmiller@Stefans-MacBook-Pro.local>
2022-03-23 23:39:29 -0400

    final solution

--- api/server.js
+++ api/server.js
@@ -5,6 +5,57 @@ const User = require('./users/model')
 const server = express()
 server.use(express.json())
 
+server.put('/api/users/:id', async (req, res) => {
+    try {
+        const possibleUser = await User.findById(req.params.id)
+        if (!possibleUser) {
+            res.status(404).json({
+                message: "the user with the specified ID does not exist"
+            })
+        } else {
+           if (!req.body.name || !req.body.bio) {
+               res.status(400).json({
+                   message: "Please provide name and bio for the user"
+               })
+           } else {
+            const updatedUser = await User.update(
+                req.params.id, 
+                req.body,
+            )   
+            res.status(200).json(updatedUser)
+           }
+        }
+    } catch (err) {
+        res.status(500).json({
+                message: "error updating user",
+                err: err.message,
+                stack: err.stack,
+        })
+    }
+})
+
+
+server.delete('/api/users/:id', async (req, res) => {
+   try {
+    const possibleUser = await User.findById(req.params.id)
+    if (!possibleUser) {
+        res.status(404).json({
+            message: "the user with the specified ID does not exist"
+        })
+    } else {
+        const deletedUser = await User.remove(possibleUser.id)
+        res.status(200).json(deletedUser)
+    }
+   } catch (err) {
+    res.status(500).json({
+        message: "There was an error while saving the user to the database",
+        err: err.message,
+        stack: err.stack,
+     })
+    }    
+})
+
+
 server.post('/api/users', (req, res) => {
     const user = req.body;
     if (!user.name || !user.bio) {
@@ -18,13 +69,14 @@ server.post('/api/users', (req, res) => {
         })
         .catch(err => {
             res.status(500).json({
-                message: "There was an error while saving the user to the database",
+                message: "error deleting user",
                 err: err.message,
                 stack: err.stack,
             })
         })
     }
 })
+
 
 server.get('/api/users', (req, res) => {
     User.find()
@@ -59,7 +111,6 @@ server.get('/api/users/:id', (req, res) => {
             })
         })
 })
-
 
 
 server.use('*', (req, res) => {
