776a4b6feddcd7127944e6394e6037e7c3c1d7f9
Stefan Miller <stefanmiller@Stefans-MacBook-Pro.local>
2022-03-23 22:29:42 -0400

    halfway

--- api/server.js
+++ api/server.js
@@ -1,7 +1,38 @@
 // BUILD YOUR SERVER HERE
 const express = require('express')
-
+const User = require('./users/model')
 const server = express()
+
+server.get('/api/users', (req, res) => {
+    User.find()
+        .then(users => {
+            res.json(users)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'error getting users',
+                err: err.message,
+                stack: err.stack,
+            })
+        })
+})
+
+
+server.get('/api/users/:id', (req, res) => {
+    User.findById(req.params.id)
+        .then(user => {
+            res.json(user)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'error getting users',
+                err: err.message,
+                stack: err.stack,
+            })
+        })
+})
+
+
 
 server.use('*', (req, res) => {
     res.status(404).json({
@@ -9,4 +40,4 @@ server.use('*', (req, res) => {
     })
 })
 
-module.exports = server; // EXPORT YOUR SERVER instead of {}
+module.exports = server;
