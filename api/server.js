46ea896d8a3974345ec58fda5572e5939715b85e
Stefan Miller <stefanmiller@Stefans-MacBook-Pro.local>
2022-03-23 23:02:29 -0400

    final two endpoints

--- api/server.js
+++ api/server.js
@@ -1,7 +1,30 @@
 // BUILD YOUR SERVER HERE
 const express = require('express')
 const User = require('./users/model')
+
 const server = express()
+server.use(express.json())
+
+server.post('/api/users', (req, res) => {
+    const user = req.body;
+    if (!user.name || !user.bio) {
+        res.status(400).json({
+            message: "Please provide name and bio for the user"
+        })
+    } else {
+    User.insert(user)
+        .then(createdUser => {
+            res.status(201).json(createdUser)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "There was an error while saving the user to the database",
+                err: err.message,
+                stack: err.stack,
+            })
+        })
+    }
+})
 
 server.get('/api/users', (req, res) => {
     User.find()
@@ -21,11 +44,16 @@ server.get('/api/users', (req, res) => {
 server.get('/api/users/:id', (req, res) => {
     User.findById(req.params.id)
         .then(user => {
+            if (!user) {
+                res.status(404).json({
+                    message: "The user with the specified ID does not exist"
+                })
+            }
             res.json(user)
         })
         .catch(err => {
             res.status(500).json({
-                message: 'error getting users',
+                message: "The user information could not be retrieved",
                 err: err.message,
                 stack: err.stack,
             })
